f23b0807f90040e1e49e00592baae41f89be9025
Strwb <42999723+Strwb@users.noreply.github.com>
2018-09-25 00:11:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,14 +4,58 @@ import tkinter.messagebox
 
 squares_showed = False
 max_scroll_v = 0
+colour_list = []
+colour_square_list = []
+
+class Colour_button:
+
+
+    def __init__(self, colour):
+        self.is_pressed = False
+        self.square_colour = colour
+
+    def press(self, event):
+        global colour_list
+        if self.is_pressed == True:
+            self.is_pressed = False
+            colour_list = self.delete_from_colour_list(colour_list, self.square_colour)
+            self.a_square.config(relief = "raised")
+        else:
+            self.is_pressed = True
+            colour_list.append(self.square_colour)
+            self.a_square.config(relief = "sunken")
+
+
+    def define_widget(self, container, r, c):
+        global colour_square_list
+        self.a_square = tk.Frame(container, width = 13, height = 13, bg = self.square_colour, borderwidth = 2, relief = "raised", padx = 1, pady = 1)
+        self.a_square.grid(row = r, column = c)
+        self.a_square.bind("<Button-1>", self.press)
+        colour_square_list.append(self)
+
+    def delete_from_colour_list(self, the_list, element):
+        return_list = []
+        for thing in the_list:
+            if thing == element:
+               continue
+            else:
+                return_list.append(thing)
+        return return_list
+
+    def reset(self):
+        self.a_square.config(relief = "raised")
 
 def show_squares(event):
     """    Prints squares and manages window size """
     global squares_showed
     global max_scroll_v
+    global colour_list
+    global colour_square_list
     row_position = 0
     col_position = 0
-   
+
+    the_button.config(relief = "raised") 
+
     try:
         row_limit = int(y_variable.get())
         col_limit = int(x_variable.get())
@@ -19,7 +63,9 @@ def show_squares(event):
         tkinter.messagebox.showerror("Złe dane", "Proszę wprowadzić rozmiary jako liczby całkowite")
         return None
 
-    if row_limit == 0 or col_limit == 0:
+    if row_limit == 0 or col_limit == 0 or len(colour_list) == 0:
+        reset_colour_squares(colour_square_list)
+        tkinter.messagebox.showerror("Brak danych", "Proszę wprowadzić rozmiary płótna i wybrać kolory")
         return None
 
     square_list = checkerboard_frame.grid_slaves()
@@ -27,14 +73,9 @@ def show_squares(event):
         square.destroy()
 
     while row_position is not row_limit:
-        colour_num = random.randint(0, 1)
 
-        if colour_num == 0:
-            colour_icon = icon_white
-        else:
-            colour_icon = icon_black
 
-        tk.Label(checkerboard_frame, width = 70, height = 70, image = colour_icon).grid(row = row_position, column = col_position)
+        tk.Frame(checkerboard_frame, width = 68, height = 68, borderwidth = 1, relief = "groove", bg = randomize_colours(colour_list)).grid(row = row_position, column = col_position)
 
         col_position += 1
         if col_position == col_limit:
@@ -42,25 +83,15 @@ def show_squares(event):
             row_position += 1
 
 
-    if col_limit <= 10:
-        canvas_width = col_limit * 74
-    else:
-        canvas_width = 740
-
-    if row_limit <= 10:
-        canvas_height = row_limit * 74 + 30
-    else:
-        canvas_height = 770
-    
+    canvas_size = canvas_size_configure(col_limit, row_limit)
     squares_showed = True
     max_scroll_v = row_limit * 74 + 30
+    top_canvas.config(width = canvas_size[0], height = canvas_size[1])
+    print(str(top_canvas.winfo_height()) + "  " + str(top_canvas.winfo_width()))
 
-
-
-    # if canvas_height
-
-    top_canvas.configure(width = canvas_width, height = canvas_height)
-
+def randomize_colours(a_list):
+    return a_list[random.randint(0, len(a_list) - 1)]
+    
 def on_frame_configure(canvas):
     top_canvas.configure(scrollregion = top_canvas.bbox("all"))
 
@@ -71,18 +102,39 @@ def roll_wheel(event):
     else:
         return None
 
+def canvas_size_configure(col_limit, row_limit):
+    canvas_width = 0
+    canvas_height = 0
 
+    if col_limit < 6:
+        canvas_width = 398
+    elif col_limit <= 10:
+        canvas_width = col_limit * 70
+    else:
+        canvas_width = 700
+    
+    if row_limit <= 10:
+        canvas_height = row_limit * 70 + 30
+    else:
+        canvas_height = 770
+    return (canvas_width, canvas_height)
+
+def reset_colour_squares(square_list):
+    
+    for element in square_list:
+        element.reset()
+ 
 root = tk.Tk()
 root.title("Program dla Werci")
 root.bind("<Return>", show_squares)
 root.bind_all("<MouseWheel>", roll_wheel)
 
-icon_white = tk.PhotoImage(file = "Magnus_W_icon.png") # imports white image
-icon_black = tk.PhotoImage(file = "Magnus_B_icon.png") # imorts black image
+# icon_white = tk.PhotoImage(file = "Magnus_W_icon.png") # imports white image
+# icon_black = tk.PhotoImage(file = "Magnus_B_icon.png") # imorts black image
 x_variable = tk.StringVar()
 y_variable = tk.StringVar() # Text variables for both entries
 
-top_canvas = tk.Canvas(root, borderwidth = 0, bg = "#ffffff")
+top_canvas = tk.Canvas(root, borderwidth = 0, bg = "#ffffff", width = 420, height = 300)
 top_frame = tk.Frame(top_canvas, bg = "#ffffff")
 vsb = tk.Scrollbar(root, orient = "vertical", command = top_canvas.yview) # vertical scroll bar
 hsb = tk.Scrollbar(root, orient = "horizontal", command = top_canvas.xview) # horizontal scroll bar
@@ -96,7 +148,6 @@ top_canvas.create_window((4, 4), window = top_frame, anchor = "nw")
 # ^ Sets up scroll bars and canvases
 
 top_frame.bind("<Configure>", lambda event, canvas = top_canvas: on_frame_configure(top_canvas)) # don't know what it does yet
-
 
 buttons_frame = tk.Frame(top_frame, bg = "#ffffff")
 buttons_frame.grid(row = 0, column = 0, sticky = tk.W)
@@ -117,14 +168,34 @@ button_frame = tk.Frame(buttons_frame, width = 50, height = 30)
 button_frame.pack_propagate(0)
 button_frame.grid(row = 0, column = 4, sticky = tk.E)
 
+colours_frame = tk.Frame(buttons_frame, bg = "#ffffff", width = 68, height = 34, padx = 10)
+colours_frame.pack_propagate(0)
+colours_frame.grid(row = 0, column = 5)
+
+colour_white = Colour_button("white")
+colour_red = Colour_button("red")
+colour_yellow = Colour_button("yellow")
+colour_purple = Colour_button("purple")
+colour_black = Colour_button("black")
+colour_green = Colour_button("green")
+colour_blue = Colour_button("blue")
+colour_orange = Colour_button("orange")
+
+colour_white.define_widget(colours_frame, 0, 0)
+colour_red.define_widget(colours_frame, 0, 1)
+colour_yellow.define_widget(colours_frame, 0, 2)
+colour_purple.define_widget(colours_frame, 0, 3)
+colour_black.define_widget(colours_frame, 1, 0)
+colour_green.define_widget(colours_frame, 1, 1)
+colour_blue.define_widget(colours_frame, 1, 2)
+colour_orange.define_widget(colours_frame, 1, 3)
+
 the_button = tk.Button(button_frame, text = "Losuj")
 the_button.bind("<Button-1>", show_squares)
 the_button.pack(expand = 1, fill = tk.BOTH)
 
 checkerboard_frame = tk.Frame(top_frame)
 checkerboard_frame.grid(row = 1, column = 0, sticky = tk.W)
-
-
 
 x_entry.focus()
 
